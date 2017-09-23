b14b6462fc526df9955733e8ecf804a2f787358a
郑俊鑫 <18810544291@163.com>
2017-09-23 11:11:11 +0800

    格式化

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ export const isBoolean = boolean => boolean === true || boolean === false || Obj
 // array instanceof Array 在多全局变量会错误，因为他依赖于 window.Array，但不同 window 下的 window.Array 是不同的
 // Array.isArray 或者 Object.prototype.toString 则可以解决多全局变量下的问题
 // 参考：http://web.mit.edu/jwalden/www/isArray.html
-export const isArray = Array.isArray || function(array) {
+export const isArray = Array.isArray || function (array) {
     return Object.prototype.toString.call(array) === '[object Array]'
 }
 
@@ -57,7 +57,7 @@ export function throttle(func, wait, options = {}) {
     let timeId, args
     let previous = 0
 
-    return function() {
+    return function () {
         args = arguments
         const now = Date.now()
 
@@ -72,7 +72,7 @@ export function throttle(func, wait, options = {}) {
             func.apply(null, args)
         }
         else if (!timeId && options.trailing !== false) {
-            timeId = setTimeout(function() {
+            timeId = setTimeout(function () {
                 // 进入 !previous && options.leading === false 的条件
                 previous = options.leading === false ? 0 : Date.now()
                 timeId = null
@@ -84,14 +84,14 @@ export function throttle(func, wait, options = {}) {
 
 export const debounce = (func, wait, immediate) => {
     let timeId, args
-    return function() {
+    return function () {
         args = arguments
 
         if (timeId) {
             clearTimeout(timeId)
         }
 
-        timeId = setTimeout(function() {
+        timeId = setTimeout(function () {
             timeId = null
             func.apply(null, args)
         }, wait)
